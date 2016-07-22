90eb678daa6cc4491fa0871b45cf64979e7449b5
prathameshcg2015 <prathameshcg2015@gmail.com>
2016-07-22 16:28:24 +0530

    Update HelloWorldController.java
    Helloindia/pratham instead of World

--- src/main/java/com/alpha/HelloWorldController.java
+++ src/main/java/com/alpha/HelloWorldController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
  
  @RequestMapping("/hello")
- public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+ public String hello(@RequestParam(value="name", required=false, defaultValue="Pratham") String name, Model model) {
   
    model.addAttribute("name", name);
    //returns the view name
-   return "helloworld";
+   return "helloindia";
  
  }
  
